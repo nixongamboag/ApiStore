1296b3ef2baab99aa14edc611a2f197d97556bc7
Nixon Gamboa Garcia <nixon.gamboa@sofka.com.co>
2023-08-09 14:44:07 -0500

    Initial Commit

---
+++ src/main/java/co/gov/metrodemedellin/crontaskmanager/CronTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package co.gov.metrodemedellin.crontaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronTaskManagerApplication.class, args);
+	}
+
+}
